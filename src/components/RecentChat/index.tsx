7736bc2c7ece3d5605cdae59623cf5f2e702c321
Daniel Sayer <daniel.sayer01@gmail.com>
2023-08-04 19:08:55 +1000

    Add working filter

---
+++ src/components/RecentChat/index.tsx
@@ -0,0 +1,29 @@
+import { Avatar, Typography } from "@mui/material"
+import { stringAvatar } from "../../utils/helperMethods/generateAvatar"
+import "./styles.scss"
+
+interface IRecentChatProps {
+  name: string
+  mostRecentMessage: string
+}
+
+const RecentChat: React.FC<IRecentChatProps> = ({
+  name,
+  mostRecentMessage,
+}): React.ReactElement => {
+  return (
+    <div className="chat-container">
+      <Avatar {...stringAvatar(name)} />
+      <div className="content">
+        <Typography variant="subtitle2" color="primary">
+          {name}
+        </Typography>
+        <Typography variant="caption" color="primary">
+          {mostRecentMessage}
+        </Typography>
+      </div>
+    </div>
+  )
+}
+
+export default RecentChat
--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,20 +1,59 @@
-import { Search } from "@mui/icons-material"
-import { InputAdornment, OutlinedInput } from "@mui/material"
+import { Search, SentimentDissatisfied } from "@mui/icons-material"
+import { InputAdornment, OutlinedInput, Typography } from "@mui/material"
 import "./styles.scss"
+import RecentChat from "../RecentChat"
+import { useState } from "react"
 
 const SideMenu: React.FC = (): React.ReactElement => {
+  const data = [
+    { name: "Jared Sexy", message: "Hello" },
+    { name: "Maxy Taxy", message: "Waddup" },
+    { name: "Jonny Boi", message: "Gangser" },
+    { name: "Max JM", message: "A hit" },
+    { name: "Jo Jo Panini", message: "Ur pretty" },
+  ]
+  const [search, setSearch] = useState<string>("")
+  const filteredData = data.filter((x) =>
+    x.name.toLowerCase().includes(search.toLowerCase())
+  )
+
   return (
     <div className="side-menu">
       <OutlinedInput
         id="Search"
         placeholder="Search"
         size="small"
+        className="mb-2"
+        value={search}
+        onChange={(e) => setSearch(e.currentTarget.value)}
         endAdornment={
           <InputAdornment position="end">
             <Search />
           </InputAdornment>
         }
       />
+      <div style={{ display: "grid", gap: "0.25rem" }}>
+        {filteredData.length > 0 ? (
+          filteredData.map((a) => (
+            <RecentChat name={a.name} mostRecentMessage={a.message} />
+          ))
+        ) : (
+          <div
+            style={{
+              opacity: 0.5,
+              display: "flex",
+              marginTop: "1rem",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="subtitle1" color="primary">
+              Looks like you have no friends
+            </Typography>
+            <SentimentDissatisfied />
+          </div>
+        )}
+      </div>
     </div>
   )
 }
