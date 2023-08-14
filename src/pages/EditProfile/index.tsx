cc26368194afdaa93f8b3be3cf5b56793ffd05fd
Daniel Sayer <daniel.sayer01@gmail.com>
2023-08-14 22:10:42 +1000

    Add basic settings page

--- src/pages/EditProfile/index.tsx
+++ src/pages/EditProfile/index.tsx
@@ -1,5 +1,89 @@
+import { Chat, Home, Info, Person, Settings, Shield } from "@mui/icons-material"
+import {
+  Button,
+  Card,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material"
+import { useContext } from "react"
+import { useNavigate } from "react-router-dom"
+import AppRoutes from "../../appRoutes"
+import Header from "../../components/Header"
+import { AuthenticationContext } from "../../context/authenticationContext"
+import "./styles.scss"
+
 const EditProfilePage: React.FC = (): React.ReactElement => {
-  return <div></div>
+  const navigate = useNavigate()
+  const { user } = useContext(AuthenticationContext)
+  const name = user?.displayName ?? ""
+  return (
+    <div className="app">
+      <Header name={name} />
+      <div className="content p-3">
+        <Card className="settings">
+          <div>
+            <Typography variant="h5">Settings</Typography>
+          </div>
+          <div className="side-menu">
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <Person />
+                  </ListItemIcon>
+                  <ListItemText primary="Account" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <Shield />
+                  </ListItemIcon>
+                  <ListItemText primary="Privacy and Security" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <Settings />
+                  </ListItemIcon>
+                  <ListItemText primary="Chat Settings" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <Chat />
+                  </ListItemIcon>
+                  <ListItemText primary="Help and Support" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <Info />
+                  </ListItemIcon>
+                  <ListItemText primary="About" />
+                </ListItemButton>
+              </ListItem>
+            </List>
+            <Button
+              variant="outlined"
+              startIcon={<Home />}
+              className="home-button w-50"
+              onClick={() => navigate(AppRoutes.getHomePage())}
+            >
+              Home
+            </Button>
+          </div>
+        </Card>
+      </div>
+    </div>
+  )
 }
 
 export default EditProfilePage
