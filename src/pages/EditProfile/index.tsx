69cc3edad7fcd6c135d6267b75519a6d177fe5e7
Daniel Sayer <daniel.sayer01@gmail.com>
2023-08-28 20:27:58 +1000

    Add image uploading

--- src/pages/EditProfile/index.tsx
+++ src/pages/EditProfile/index.tsx
@@ -1,6 +1,7 @@
 import {
   Chat,
   ChevronLeft,
+  Edit,
   Info,
   Person,
   Settings,
@@ -17,18 +18,41 @@ import {
   ListItemText,
   Typography,
 } from "@mui/material"
-import { useContext } from "react"
+import { ChangeEvent, useContext } from "react"
 import { useNavigate } from "react-router-dom"
 import AppRoutes from "../../appRoutes"
 import Header from "../../components/Header"
 import { AuthenticationContext } from "../../context/authenticationContext"
 import "./styles.scss"
 import { stringAvatar } from "../../utils/helperMethods/generateAvatar"
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage"
+import { storage } from "../../firebase"
+import { updateProfile } from "firebase/auth"
 
 const EditProfilePage: React.FC = (): React.ReactElement => {
   const navigate = useNavigate()
   const { user } = useContext(AuthenticationContext)
   const name = user?.displayName ?? ""
+
+  const handleAddImage = async (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files?.length || !user) return
+    const file = e.target.files[0]
+    const storageRef = ref(storage, name)
+
+    try {
+      const uploadTask = uploadBytesResumable(storageRef, file)
+      await uploadTask
+      const downloadUrl = await getDownloadURL(storageRef)
+
+      await updateProfile(user, {
+        displayName: name,
+        photoURL: downloadUrl,
+      })
+    } catch (error) {
+      console.error("Error uploading image:", error)
+    }
+  }
+
   return (
     <div className="app">
       <Header name={name} />
@@ -94,7 +118,22 @@ const EditProfilePage: React.FC = (): React.ReactElement => {
               </List>
             </div>
             <div className="menu-option">
-              <Avatar {...stringAvatar(name)} />
+              <div style={{ position: "relative" }}>
+                <Avatar className="avatar" {...stringAvatar(name)} />
+                <label htmlFor="file-select">
+                  <Edit className="cursor-pointer" />
+                </label>
+                <input
+                  id="file-select"
+                  type="file"
+                  style={{ display: "none" }}
+                  accept="image/jpeg, image/png, image/jpg"
+                  onChange={handleAddImage}
+                />
+              </div>
+              <Typography variant="h5">Welcome, {name}</Typography>
+
+              <img src={user?.photoURL ?? ""} />
             </div>
           </div>
         </Card>
